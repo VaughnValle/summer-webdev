dd64fc8b4050601ed29ba65be2d5ae4e25ad1fee
VaughnValle <vaughnmatthewvalle@gmail.com>
2024-06-13 22:22:56 -0400

    Switched font to Helvetica + color to gray

--- eg1004-announcement/tailwind.config.mjs
+++ eg1004-announcement/tailwind.config.mjs
@@ -6,8 +6,12 @@ export default {
         colors: {
             'purple': '#57068c',
             'white': '#ffffff',
+            'black': '#333333',
             'gray': '#F5F5F5',
             'grayer': '#dbdbdb',
+        },
+        fontFamily: {
+            'sans':['Helvetica Neue','Helvetica','Arial','sans-serif'],
         },
 	},
 	plugins: [],
