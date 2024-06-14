65c2d39affd8fa229d12247ac2771bc611a4fdfd
VaughnValle <vaughnmatthewvalle@gmail.com>
2024-06-13 21:47:24 -0400

    Added sidebar as flex item

--- eg1004-announcement/src/components/Sidebar.jsx
+++ eg1004-announcement/src/components/Sidebar.jsx
@@ -3,12 +3,11 @@ import Card from './Card.jsx';
 
 const Sidebar = () => {
     return (
-        <>
+        <div className='sticky flex-column'>
             <Card title='Course Information' content='Sample'/>
             <Card title='Employees' content='Sample'/>
             <Card title='Help Center' content='Sample'/>
-
-        </>
+        </div>
     )
 };
 
