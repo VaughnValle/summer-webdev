ba482dab2ed7833efe2f53d559b9c42e2d971ae2
VaughnValle <vaughnmatthewvalle@gmail.com>
2024-06-13 22:25:24 -0400

    Changed Cards padding and font

--- eg1004-announcement/src/components/AnnounceCard.jsx
+++ eg1004-announcement/src/components/AnnounceCard.jsx
@@ -7,12 +7,12 @@ const Card = ({
     time,
 }) => {
     return (
-        <div className='mx-auto block max-w-5xl p-0 border border-purple rounded-lg'>
-            <div className='bg-purple p-2 rounded-lg rounded-b-none'>
-                <h5 class='mb-2 text-lg font-normal tracking-tight text-white dark:text-white'>{title}</h5>
+        <div className='mx-auto block max-w-5xl p-0 border border-purple rounded-md'>
+            <div className='bg-purple p-4 py-2 rounded-md rounded-b-none'>
+                <h5 class='mb-2 text-lg font-light tracking-tight text-black dark:text-white'>{title}</h5>
             </div>
-            <p class='font-normal p-2'>{content}</p>
-            <footer className='bg-gray border-t border-grayer p-2'><p><strong>Posted on:</strong> {date} at {time}</p></footer>
+            <p class='font-light p-4 leading-10'>{content}</p>
+            <footer className='bg-gray border-t border-grayer p-4'><p><strong>Posted on:</strong> {date} at {time}</p></footer>
         </div>
 
     )
--- eg1004-announcement/src/components/Card.jsx
+++ eg1004-announcement/src/components/Card.jsx
@@ -7,9 +7,9 @@ const Card = ({
     time,
 }) => {
     return (
-        <div className='block mx-auto max-w-sm p-0 bg-white border border-purple rounded-lg'>
-            <div className='bg-purple p-2 rounded-lg rounded-b-none'>
-                <h5 class='mb-2 text-lg font-normal tracking-tight text-white dark:text-white'>{title}</h5>
+        <div className='block mx-auto max-w-sm p-0 bg-white border border-purple rounded-md'>
+            <div className='bg-purple p-2 rounded-md rounded-b-none'>
+                <h5 class='mb-2 text-lg font-thin tracking-tight text-black dark:text-white'>{title}</h5>
             </div>
             <p class='font-normal p-2'>{content}</p>
         </div>
