6ddc6a5ab57adc1c95afcd36eae964daf176d4ba
VaughnValle <vaughnmatthewvalle@gmail.com>
2024-06-13 22:58:55 -0400

    Remove redundant props

--- eg1004-announcement/src/components/AnnounceCard.jsx
+++ eg1004-announcement/src/components/AnnounceCard.jsx
@@ -8,7 +8,7 @@ const Card = ({
 }) => {
     return (
         <div className='mx-auto block max-w-5xl p-0 border border-purple rounded-md'>
-            <div className='bg-purple p-4 py-2 rounded-md rounded-b-none'>
+            <div className='bg-purple p-4 py-2 pb-1 rounded-md rounded-b-none'>
                 <h5 class='mb-2 text-lg font-light tracking-tight text-black dark:text-white'>{title}</h5>
             </div>
             <p class='font-light p-4 leading-10'>{content}</p>
--- eg1004-announcement/src/components/Card.jsx
+++ eg1004-announcement/src/components/Card.jsx
@@ -3,12 +3,10 @@ import React from 'react';
 const Card = ({
     title,
     content,
-    date,
-    time,
 }) => {
     return (
         <div className='block mx-auto mb-5 max-w-sm p-0 bg-white border border-purple rounded-md'>
-            <div className='bg-purple p-2 rounded-md rounded-b-none'>
+            <div className='bg-purple p-4 pl-2 py-1 pb-0 rounded-md rounded-b-none'>
                 <h5 class='mb-2 text-lg font-thin tracking-tight text-black dark:text-white'>{title}</h5>
             </div>
             <p class='font-normal p-2'>{content}</p>
