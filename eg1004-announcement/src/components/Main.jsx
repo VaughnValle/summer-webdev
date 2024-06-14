74b1b472b0ecd453c5ea64139fb76e2078d2dcb7
VaughnValle <vaughnmatthewvalle@gmail.com>
2024-06-13 21:29:50 -0400

    Layout cards with flex

--- eg1004-announcement/src/components/Main.jsx
+++ eg1004-announcement/src/components/Main.jsx
@@ -4,15 +4,17 @@ import AnnounceCard from './AnnounceCard.jsx';
 
 const Main = () => {
     return (
-        <>
-            <h1 className='text-2xl'>Welcome to General Engineering 1004</h1>
+        <div className='mx-auto w-9/12 flex-column flex-auto items-center justify-center'>
+            
+            <h1 className='text-2xl mx-auto ontent-center'>Welcome to General Engineering 1004</h1>
             <AnnounceCard 
                 title='Congratulations to our showcase winners!' 
                 content='Dear Class. I hope finals are treating you well.'
                 date='May 13, 2024'
                 time='10:51 AM'
+                className='mx-auto'
             />
-        </>
+        </div>
     )
 };
 
--- eg1004-announcement/src/components/Navbar.jsx
+++ eg1004-announcement/src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 const logo='logo.png';
 
 const Navbar = () => {
